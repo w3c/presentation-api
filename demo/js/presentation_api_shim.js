13860dc577b16d48691f768cce7946aced2cfc44
Dominik Röttsches <dominik.rottsches@intel.com>
2013-12-13 13:47:27 +0100

    Namespacing and shortening of the shim code according to Anssi's suggestions.

--- demo/js/presentation_api_shim.js
+++ demo/js/presentation_api_shim.js
@@ -1,20 +1,15 @@
-function presentationApi() {
-}
-
-presentationApi.prototype.tryOpen = function(url, resolve, reject) {
-    presentationWindow = window.open(url, "", "presentation");
-    if (presentationWindow)
-	resolve(presentationWindow);
-    else
-	reject();
-}
-
-presentationApi.prototype.requestShow = function(url) {
-    var that = this;
-    function tryOpenWithUrl(resolve, reject) {
-	that.tryOpen(url, resolve, reject);
+(function() {
+    function Presentation() {
     }
-    return new Promise(tryOpenWithUrl);
-}
 
-navigator.presentation = new presentationApi();
+    Presentation.prototype.requestShow = function(url) {
+        return new Promise(function(resolve, reject) {
+            presentationWindow = window.open(url, "", "presentation");
+            if (presentationWindow)
+                resolve(presentationWindow);
+            else
+                reject();
+        });
+    };
+    navigator.presentation = new Presentation();
+}());
